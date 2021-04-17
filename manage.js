fe54c4a8172e049c48b1474eb857d9aa6710bd20
mcbridz@gmail.com <mcbridz@gmail.com>
2021-04-16 18:29:04 -0700

    It works, adding new json files and writing to existing one

--- manage.js
+++ manage.js
@@ -6,29 +6,31 @@ http.createServer((req, res) => {
     console.log(path)
     let body = []
     req.on('data', chunk => {
-        console.log('ADDING CHUNK')
+        // console.log('ADDING CHUNK')
         body.push(chunk)
-        console.log(body)
-        console.log(`Currently ${body.length} chunks accumulated.`)
+        // console.log(body)
+        // console.log(`Currently ${body.length} chunks accumulated.`)
     })
-    console.log(body)
+    // console.log(body)
     req.on('end', () => {
         console.log('END TRIGGERED')
-        body = Buffer.concat(body).toString()
+        // body = Buffer.concat(body).toString()
         console.log(body)
         if (req.method === 'POST') {
-            console.log('POST')
+            // console.log('POST')
             console.log(path.pathname)
             if (path.pathname === '/messages') {
-                console.log('/messages')
+                // console.log('/messages')
                 msgObj = JSON.parse(body)
+                console.log(msgObj)
                 let date = new Date()
-                let filename = date.getMonth.toString() + date.getDay.toString() + date.getFullYear.toString()
+                let filename = date.getMonth().toString() + date.getDay().toString() + date.getFullYear().toString()
+                console.log(filename)
                 jsonManagement.addMessageTo(filename, msgObj)
                 res.end(`Added message object to ${filename}.json`)
             }
         }
-        res.end('No processing of request occurred.')
+        else res.end('No processing of request occurred.')
     })
 }).listen(port, () => {
     console.log(`Listening on port: ${port}`)
--- utils/jsonManagement.js
+++ utils/jsonManagement.js
@@ -1,20 +1,25 @@
 const fs = require('fs')
 function addMessageTo(filename, msgObj) {
-    let pathandFile = `/messages/${filename}`
+    let pathandFile = __dirname + `/../messages/${filename}.json`
     if (!fs.existsSync(pathandFile)) {
         let firstMessage = { msgObj }
         let date = new Date()
-        let newfilename = date.getMonth.toString() + date.getDay.toString() + date.getFullYear.toString()
-        fs.appendFile('/message/' + newfilename + '.json', JSON.stringify(firstMessage), (err) => {
-            if (err) throw err
-            console.log(`File created at ${newfilename}`)
+        let newfilename = date.getMonth().toString() + date.getDay().toString() + date.getFullYear().toString()
+        fs.writeFile(__dirname + '/../messages/' + newfilename + '.json', JSON.stringify([firstMessage]), (err) => {
+            if (err) {
+                console.log(err)
+                throw err
+            }
+            console.log(`File created at ${newfilename}.json`)
         })
     } else {
         fs.readFile(pathandFile, (err, data) => {
             let messages = JSON.parse(data)
             messages.push(msgObj)
-            fs.writeFile(pathandFile, JSON.stringify(messages))
+            fs.writeFile(pathandFile, JSON.stringify(messages), (err) => {
+                if (err) throw err
+            })
         })
     }
-}
+}//C:\Users\mcbri\Documents\code\node_chatroom\messages
 module.exports = { addMessageTo }
